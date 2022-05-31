502211dd181bff61b4911cc83ecad60f6c1b949b
Nikola Mitev <mitev.nkl@gmail.com>
2022-05-31 14:10:53 +0200

    asdasdf

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module COACHING4LIFE
       generate.test_framework :test_unit, fixture: false
     end
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1.6
+    config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
