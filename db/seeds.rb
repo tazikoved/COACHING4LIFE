069ce4fae6f69f68f47248811e3edfc0aa0ed0ca
Mael Micout <micoutmael3@gmail.com>
2022-05-31 12:09:36 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create([ first_name: "Mael",  last_name: "Micout", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: true, refugee: false ])
+users = User.create([ first_name: "Barbara",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: true, refugee: false ])
+users = User.create([ first_name: "Star Wars",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: true, refugee: false ])
+users = User.create([ first_name: "Star Wars",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: true, refugee: false ])
+users = User.create([ first_name: "Star Wars",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: true, refugee: false ])
+users = User.create([ first_name: "Star Wars",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: true, refugee: false ])
+users = User.create([ first_name: "Star Wars",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: true, refugee: false ])
+
+users = User.create([ first_name: "Star Wars",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: false, refugee: true ])
+users = User.create([ first_name: "Star Wars",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: false, refugee: true ])
+users = User.create([ first_name: "Star Wars",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: false, refugee: true ])
+users = User.create([ first_name: "Star Wars",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: false, refugee: true ])
+users = User.create([ first_name: "Star Wars",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: false, refugee: true ])
+users = User.create([ first_name: "Star Wars",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: false, refugee: true ])
+users = User.create([ first_name: "Star Wars",  last_name: "Lord of the Rings", email: "mael@gmailcom", password: "Lewagon906", description: "abdazbdazopfaozfbiaf", coach: false, refugee: true ])
