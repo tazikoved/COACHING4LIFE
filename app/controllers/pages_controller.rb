5d9c24031d01fb6bf1e2bd349bd8fc5492ae17d3
Denis Gavrilenco <denis.gavrilenco@gmail.com>
2022-06-02 14:11:29 +0200

    conflict solved

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :ui_kit ]
+  # skip_before_action :authenticate_user!, only: [ :home, :ui_kit ]
 
   def home
   end
